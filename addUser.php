2fd7da6d35137524919aa20e236ac4b9b95b1de2
betsy_liu <40143204@gm.nfu.edu.tw>
2016-08-26 02:37:08 +0000

    api練習:新增判斷參數數量

--- addUser.php
+++ addUser.php
@@ -1,9 +1,20 @@
 <?php
 require_once("Database.php");
+header("Content-Type:text/html; charset=utf-8");
 
 $userName = $_GET['username'];
 
 $db = new Database();
+
+if ($userName == null) {
+    $parameterFail = array(
+        "result" => "false",
+        "message" => "quantity of parameter is wrong"
+    );
+
+    echo json_encode($parameterFail);
+    exit;
+}
 
 $sqlInsertUser = "INSERT INTO `user`(`userName`)
                   VALUES ('$userName')";
@@ -17,20 +28,20 @@ if ($userName != "" && $resSelectUser[0]['userName'] != $userName ) {
     $resInsertUser = $db -> insert($sqlInsertUser);
 }
 
-$success = array(
-    "result" => "true",
-    "username" => $userName
-);
-
-$fail = array(
-    "result" => "false",
-    "message" => "username repeat"
-);
-
 if ($resInsertUser == true) {
+    $success = array(
+        "result" => "true",
+        "username" => $userName
+    );
+
     echo json_encode($success);
 }
 
 if ($resSelectUser[0]['userName'] == $userName ) {
+    $fail = array(
+        "result" => "false",
+        "message" => "username repeat"
+    );
+
     echo json_encode($fail);
 }
