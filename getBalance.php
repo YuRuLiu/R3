69c8f343d09c424993fd2261aae07b308c86083f
betsy_liu <40143204@gm.nfu.edu.tw>
2016-08-26 02:38:14 +0000

    api練習:新增判斷參數數量

--- getBalance.php
+++ getBalance.php
@@ -5,26 +5,33 @@ header("Content-Type:text/html; charset=utf-8");
 $db = new Database();
 $userName = $_GET['username'];
 
+if ($userName == null) {
+    $parameterFail = array(
+        "result" => "false",
+        "message" => "quantity of parameter is wrong"
+    );
+
+    echo json_encode($parameterFail);
+    exit;
+}
+
 $sqlSelectBalance = "SELECT `userName`, `balance`
                      FROM `user`
                      WHERE `userName` = '$userName'";
 $resSelectBalance = $db -> select($sqlSelectBalance);
 
-
-$success = array(
-    "result" => "true",
-    "balance" => $resSelectBalance[0]['balance']
-);
-
-$fail = array(
-    "result" => "false",
-    "message" => "this user does not exist"
-);
-
 if ($resSelectBalance[0]['userName'] != "") {
+    $success = array(
+        "result" => "true",
+        "balance" => $resSelectBalance[0]['balance']
+    );
     echo json_encode($success);
 }
 
 if ($resSelectBalance[0]['userName'] == "") {
+    $fail = array(
+        "result" => "false",
+        "message" => "this user does not exist"
+    );
     echo json_encode($fail);
 }
